432f5728678c6caa257fdc9c7db717b9ff183421
beatriz.xavier <beatriz.xavier@ifood.com.br>
2022-07-13 09:58:35 -0300

    indentei o app.js

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 //importei o express para o app.js
-const express = require("express")
+const express = require("express");
 
 //executando o servidor express
-const server = express()
+const server = express();
 
 //configurando para o servidor responder em .json
-server.use(express.json())
+server.use(express.json());
 
 //acordo coletivo para comunicações http
-server.listen(8080,() => console.log("servidor online"))
+server.listen(8080, () => console.log("servidor online"));
 
- //endpoints
- server.get("/",(req,res) =>{res.send("olá, mundo! :)")})
+//endpoints
+server.get("/", (req, res) => {
+  res.send("olá, mundo! :)");
+});
 
- server.get("/nome",function (req,res) {
-    res.status(200).send("beatriz :)")
-})
+server.get("/nome", function (req, res) {
+  res.status(200).send("beatriz :)");
+});
