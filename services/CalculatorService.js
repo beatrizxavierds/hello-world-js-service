caabd4eca500c984d347b9bfbe8969a82a7c1cf2
beatriz.xavier <beatriz.xavier@ifood.com.br>
2022-07-29 16:10:37 -0300

    feat: adicionei a função de subtração

--- services/CalculatorService.js
+++ services/CalculatorService.js
@@ -2,4 +2,10 @@ function somar(numero1, numero2) {
   let numero3 = numero1 + numero2;
   return numero3;
 }
-module.exports = { somar };
+
+function subtrair(numeroA, numeroB) {
+  let numeroSub = numeroA - numeroB;
+  return numeroSub;
+}
+
+module.exports = { somar, subtrair };
