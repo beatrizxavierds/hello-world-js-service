93d86848b269b1509d17b0772631ab067211afb1
beatriz.xavier <beatriz.xavier@ifood.com.br>
2022-08-05 15:59:39 -0300

    feat: criei a função de dividir na CalculatorService.js

--- services/CalculatorService.js
+++ services/CalculatorService.js
@@ -8,4 +8,26 @@ function subtrair(numeroA, numeroB) {
   return resultado;
 }
 
-module.exports = { somar, subtrair };
+function dividir(numeroA, numeroB) {
+  //verificar se numeroB é = a zero
+  if (numeroB === 0) {
+    //se for zero, retornar um erro
+    return "Erro";
+  }
+
+  //se for diferente de zero, fazer o cálculo
+  let resultado = numeroA / numeroB;
+  return resultado;
+}
+
+// function dividir2(numeroA, numeroB) {
+//   //verificar se numeroB é diferente de zero
+//   if (numeroB != 0) {
+//     //se for diferente de zero, fazer o cálculo
+//     let resultado = numeroA / numeroB;
+//     return resultado;
+//   }
+//   //se for zero, retornar um erro
+//   return "Erro";
+//}
+module.exports = { somar, subtrair, dividir };
